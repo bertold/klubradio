b80f8975eddd986599917186e1adc8e71424e44f
Bertold Kolics <bkolics@verosint.com>
2025-12-31 11:15:54 -0600

    update feed

--- update-feed.sh
+++ update-feed.sh
@@ -29,7 +29,7 @@ if [ -z "$FILE_PREFIX" ]; then
   exit 1
 fi
 
-STREAM_URL="https://www.klubradio.hu/data/hanganyagok/$(date +%Y/%-m/%-d)/archivum_alenyeg${FILE_PREFIX}_$(date +%y%m%d).mp3"
+STREAM_URL="https://www.klubradio.hu/data/hanganyagok/$(date +%Y/%-m/%-d)/archivum_alenyeg_${FILE_PREFIX}_$(date +%y%m%d).mp3"
 USER_AGENT=$(curl -s https://raw.githubusercontent.com/jnrbsn/user-agents/refs/heads/main/user-agents.json | jq '.[1]')
 if [ -z "$USER_AGENT" ]; then
   USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
@@ -46,7 +46,7 @@ fi
 curl --output /dev/null --header "User-Agent: ${USER_AGENT}" --silent --fail "${STREAM_URL}"
 RC=$?
 if [ ${RC} -ne 0 ]; then
-  echo "No updated stream found at either ${STREAM_URL}. Status code: ${RC}"
+  echo "No updated stream found at ${STREAM_URL}. Status code: ${RC}"
   exit 0
 fi
 
